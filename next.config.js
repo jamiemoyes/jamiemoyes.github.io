e782dd16ab09354e005ac49cb94900a4a07cee9c
Jamie Moyes <jamie.moyes@sky.uk>
2023-11-02 19:48:34 +0000

    update config

--- next.config.js
+++ next.config.js
@@ -1,8 +1,4 @@
-/**
- * @type {import('next').NextConfig}
- */
-const nextConfig = {
-  output: "export",
-};
+/** @type {import('next').NextConfig} */
+const nextConfig = {};
 
 module.exports = nextConfig;
