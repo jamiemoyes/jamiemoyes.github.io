bf3c6f06220796a6e0fe4c307a3a9198eef7bb97
Jamie Moyes <jamie.moyes@sky.uk>
2023-11-08 20:57:48 +0000

    add next export config

--- next.config.js
+++ next.config.js
@@ -1,21 +1,22 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "images.prismic.io",
-        port: "",
-        pathname: "/moyes-portfolio/**",
-      },
-      {
-        protocol: "https",
-        hostname: "i.scdn.co",
-        port: "",
-        pathname: "/image/**",
-      },
-    ],
-  },
+  output: "export",
+  // images: {
+  //   remotePatterns: [
+  //     {
+  //       protocol: "https",
+  //       hostname: "images.prismic.io",
+  //       port: "",
+  //       pathname: "/moyes-portfolio/**",
+  //     },
+  //     {
+  //       protocol: "https",
+  //       hostname: "i.scdn.co",
+  //       port: "",
+  //       pathname: "/image/**",
+  //     },
+  //   ],
+  // },
 };
 
 module.exports = nextConfig;
