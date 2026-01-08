e20a739ddd2fe7077cfd42499ccdd73f578c1d10
jamie-moyes-depop <jamie.moyes@depop.com>
2026-01-08 20:37:30 +0000

    add types

--- src/app/shared/types/types.ts
+++ src/app/shared/types/types.ts
@@ -4,7 +4,7 @@ export enum Colour {
   Purple = "purple",
   Blue = "blue",
   LightBlue = "light-blue",
-  Green = 'green'
+  Green = "green",
 }
 
 export interface TrackInfo {
@@ -14,8 +14,61 @@ export interface TrackInfo {
   images: Image[];
 }
 
-export interface Image {
-  height: number;
+// export interface Image {
+//   height: number;
+//   url: string;
+//   width: number;
+// }
+
+export interface LastFmResponse {
+  recenttracks: Recenttracks;
+}
+
+export interface Recenttracks {
+  track: Track[];
+  "@attr": Attr2;
+}
+
+export interface Track {
+  artist: Artist;
+  streamable: string;
+  image: Image[];
+  mbid: string;
+  album: Album;
+  name: string;
+  "@attr"?: Attr;
   url: string;
-  width: number;
+  date?: Date;
+}
+
+export interface Artist {
+  mbid: string;
+  "#text": string;
+}
+
+export interface Image {
+  size: string;
+  "#text": string;
+}
+
+export interface Album {
+  mbid: string;
+  "#text": string;
+}
+
+export interface Attr {
+  nowplaying: string;
+}
+
+export interface Date {
+  uts: string;
+  "#text": string;
+}
+
+export interface Attr2 {
+  user: string;
+  totalPages: string;
+  page: string;
+  perPage: string;
+  total: string;
 }
